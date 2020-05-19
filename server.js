547d3550269668728bce9154944a9d4a7cbd87d5
KIKOmanasijev <manasijevhristijan@gmail.com>
2020-05-20 01:41:57 +0200

    Passport integrated

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require('passport')
 
-const users = require('./routes/api/users');
-const posts = require('./routes/api/posts');
-const profile = require('./routes/api/profile');
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const profile = require("./routes/api/profile");
 
 const app = express();
 
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //DB CONFIG
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //Connect to MONGODB
 mongoose
-    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("mongodb connected"))
-    .catch((err) => console.log(err));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Hello!!');
-});
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 //Use routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
